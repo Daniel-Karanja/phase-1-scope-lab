92023fb9b047393578ff6baae919481b095be169
Arthur-codeX <daniel.karanja239@gmail.com>
2022-03-06 00:37:34 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Write your solution in this file!
+var customerName = "bob";
+var bestCustomer = "";
+const leastFavoriteCustomer = "";
+
+const upperCaseCustomerName = () => {
+  customerName.toUpperCase();
+};
+const setBestCustomer = () => {
+  bestCustomer = "not bob";
+};
+
+const overwriteBestCustomer = () => {
+  bestCustomer = "changes";
+};
+
+const changeLeastFavoriteCustomer = () => {
+  leastFavoriteCustomer = "not bob";
+};
